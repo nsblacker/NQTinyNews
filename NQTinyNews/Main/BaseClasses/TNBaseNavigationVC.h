c75a1a4b8d6ae01e1c6685c6cf01adc070895f1e
nsblacker <nsblacker.163.com>
2017-06-19 20:25:15 +0800

    cell
    主页cell约束

---
+++ NQTinyNews/Main/BaseClasses/TNBaseNavigationVC.h
@@ -0,0 +1,13 @@
+//
+//  TNBaseNavigationVC.h
+//  NQTinyNews
+//
+//  Created by NaQiang_iMac on 2017/6/19.
+//  Copyright © 2017年 那强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TNBaseNavigationVC : UINavigationController
+
+@end
---
+++ NQTinyNews/Main/BaseClasses/TNBaseVC.h
@@ -0,0 +1,13 @@
+//
+//  TNBaseVC.h
+//  NQTinyNews
+//
+//  Created by NaQiang_iMac on 2017/6/19.
+//  Copyright © 2017年 那强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TNBaseVC : UIViewController
+
+@end
