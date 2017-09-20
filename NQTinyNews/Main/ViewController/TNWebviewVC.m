d7a1e59e5c6577d73a2f46639c2723b82ce64dbf
nsblacker <nsblacker.163.com>
2017-09-20 15:54:20 +0800

    首页新闻

--- NQTinyNews/Main/ViewController/TNWebviewVC.m
+++ NQTinyNews/Main/ViewController/TNWebviewVC.m
@@ -10,20 +10,58 @@
 #import <WebKit/WebKit.h>
 
 @interface TNWebviewVC ()<WKUIDelegate,WKNavigationDelegate>
-
+@property(nonatomic ,strong)WKWebView *webView;
+@property(nonatomic ,strong)UIProgressView *progressView;
 @end
 
 @implementation TNWebviewVC
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
-    WKWebView *webview = [[WKWebView alloc]initWithFrame:self.view.bounds];
-    [webview loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:_urlStr]]];
-    [self.view addSubview:webview];
+    [self.view addSubview:self.webView];
+    [self.view addSubview:self.progressView];
+    [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:_urlStr]]];
 }
 
-#pragma -mark WKWebview Delegate
+#pragma -mark -lazy UI
+-(WKWebView*)webView{
+    if(!_webView){
+        _webView = [[WKWebView alloc]initWithFrame:self.view.bounds];
+        _webView.navigationDelegate = self;
+        _webView.UIDelegate = self;
+        [_webView addObserver:self forKeyPath:@"estimatedProgress" options:NSKeyValueObservingOptionNew context:nil];
+    }
+    return _webView;
+}
 
+-(UIProgressView*)progressView{
+    if(!_progressView){
+        _progressView = [[UIProgressView alloc] initWithFrame:CGRectMake(0,64, ScreenWidth, 0)];
+        self.progressView.tintColor = [UIColor redColor];
+        self.progressView.trackTintColor = [UIColor whiteColor];
+    }
+    return _progressView;
+}
+
+#pragma -mark KVO
+// 计算wkWebView进度条
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    if (object == self.webView && [keyPath isEqualToString:@"estimatedProgress"]) {
+        CGFloat newprogress = [[change objectForKey:NSKeyValueChangeNewKey] doubleValue];
+        if (newprogress == 1) {
+            self.progressView.hidden = YES;
+            [self.progressView setProgress:0 animated:NO];
+        }else {
+            self.progressView.hidden = NO;
+            [self.progressView setProgress:newprogress animated:YES];
+        }
+    }
+}
+
+- (void)dealloc {
+    [self.webView removeObserver:self forKeyPath:@"estimatedProgress"];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
