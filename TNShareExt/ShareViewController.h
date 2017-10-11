34f84b5bdd1087067963bd394bedd93c2de78b39
nsblacker <nsblacker.163.com>
2017-10-11 12:25:18 +0800

    extension
    添加extension

---
+++ TNShareExt/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  TNShareExt
+//
+//  Created by NaQiang_iMac on 2017/10/9.
+//  Copyright © 2017年 那强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
